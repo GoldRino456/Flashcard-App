56e1b648014c6b6f30b4c854c5f333027fd4f7e9
Ethan E. <eastwoode375@gmail.com>
2025-06-17 21:35:01 -0500

    Table Creation Functional

---
+++ Flashcards.GoldRino456/DatabaseManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Utilities.GoldRino456;
+
+namespace Flashcards.GoldRino456
+{
+    internal class DatabaseManager
+    {
+        //Reusable SQL Commands
+        
+
+        private readonly string _connectionString;
+
+        public DatabaseManager()
+        {
+            FetchConnectionString(out _connectionString);
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            //Create Stacks Table
+            DatabaseUtils.ExecuteNonQueryCommand(_connectionString, "IF OBJECT_ID(N'Stacks', N'U') IS NULL CREATE TABLE Stacks (stackID INT IDENTITY(1,1) PRIMARY KEY, stackName VARCHAR(50));");
+
+            //Create Cards Table
+            DatabaseUtils.ExecuteNonQueryCommand(_connectionString, "IF OBJECT_ID(N'Cards', N'U') IS NULL CREATE TABLE Cards (cardID INT IDENTITY(1,1) PRIMARY KEY, stackID INT FOREIGN KEY REFERENCES Stacks(stackID), frontOfCard VARCHAR(500), backOfCard VARCHAR(500));");
+
+            //Create Sessions Table
+            DatabaseUtils.ExecuteNonQueryCommand(_connectionString, "IF OBJECT_ID(N'Sessions', N'U') IS NULL CREATE TABLE Sessions (sessionID INT IDENTITY(1,1) PRIMARY KEY, stackID INT FOREIGN KEY REFERENCES Stacks(stackID), sessionDate DATE, backOfCard VARCHAR(500));");
+        }
+
+        private void FetchConnectionString(out string connectionString)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                        .AddJsonFile("appSettings.json")
+                        .Build();
+
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
--- Flashcards.GoldRino456/Program.cs
+++ Flashcards.GoldRino456/Program.cs
@@ -1,17 +1,3 @@
-﻿using Microsoft.Extensions.Configuration;
-using Dapper;
-using Microsoft.Data.SqlClient;
+﻿using Flashcards.GoldRino456;
 
-IConfiguration config = new ConfigurationBuilder()
-    .AddJsonFile("appSettings.json")
-    .Build();
-
-var connectionString = config.GetConnectionString("DefaultConnection");
-Console.WriteLine(connectionString);
-
-var connection = new SqlConnection(connectionString);
-connection.Open();
-
-var query = connection.QuerySingle<string>("SELECT DB_NAME(db_id())");
-
-Console.WriteLine(query);
+var databaseManager = new DatabaseManager();
